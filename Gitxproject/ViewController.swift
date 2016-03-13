08799470a3245d8fc48a4c6da4bbf765c9221adb
Park Jung Hyun <spawn622@Parkui-MacBook-Pro.local>
2016-03-13 17:25:11 +0900

    create project

---
+++ Gitxproject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Gitxproject
+//
+//  Created by Park Jung Hyun on 2016. 3. 13..
+//  Copyright © 2016년 Park Jung Hyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("이것이 깃의정석이요내가다알려주겠다.")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
